d02df958ba1f313d585fe280def0b7b40de4cb38
V.G. Johne <vgj21@mlsalt-cpu2.eng.cam.ac.uk>
2016-02-17 22:31:44 +0000

    test gp

---
+++ regression/svm.py
@@ -0,0 +1,16 @@
+import numpy as np
+from sklearn.svm import SVR
+
+
+#tmp = 'silly/Kaggle/regression'
+X = np.load('Data/reg_train_in.npy')
+y = np.load('Data/reg_train_out.npy')
+y = [x[0] for x in y]
+y=np.array(y)
+test_in = np.load('Data/reg_test_in.npy')
+
+clf = SVR(C=1.0, epsilon =0.2)
+clf.fit(X,y)
+test_out = clf.predict(test_in)
+np.save(test_out, 'svm')
+np.savetext(test_out, 'svm')
